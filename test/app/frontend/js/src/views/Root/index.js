4482c10bb03080833d8c70f37b6bb1d23f2ed016
Mark McKelvy <mmckelvy@gmail.com>
2023-04-30 17:50:47 -0500

    more dev work

--- test/app/frontend/js/src/views/Root/index.js
+++ test/app/frontend/js/src/views/Root/index.js
@@ -2,8 +2,32 @@ import React from 'react';
 
 import './style.css';
 
+import useForm from '../../../../../../../src/index.js';
+
 export default function Root() {
+  const { fields, setFields, handleChange } = useForm({
+    order: {
+      id: {
+        value: '',
+      },
+      items: [
+        {
+          productNumber: {
+            value: ''
+          },
+          quantity: ''
+        },
+      ]
+    }
+  });
+
   return (
-    <div>Hello world!</div>
+    <div>
+      <input
+        name="order.id"
+        value={fields.order.id.value}
+        onChange={handleChange}
+      />
+    </div>
   );
 }
